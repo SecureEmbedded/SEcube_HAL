8cfadf938d66e2ec6a6a18e87e01fe8b1433ef3a
SecureEmbedded <salvatoredesimone.embedded@gmail.com>
2016-11-22 10:17:28 +0100

    Added a first implementation of SD writing, but it not works and the function cannot access to the disk.

--- Inc/fatfs.h
+++ Inc/fatfs.h
@@ -15,6 +15,17 @@
 #include "ff_gen_drv.h"
 #include "sd_diskio.h" /* defines SD_Driver as external */
 
+/* Lets define a buffer of DIM_BUFFER bytes */
+#define DIM_BUFFER 1024
+
+/* Access */
+#define CREATE_NEW 		FA_CREATE_NEW
+#define CREATE_ALWAYS 	FA_CREATE_ALWAYS
+
+/* Operations */
+#define RD FA_READ
+#define WR FA_WRITE
+
 extern uint8_t retSD; /* Return status for SD
  	 	 	 	 	 	 0 = OK
  	 	 	 	 	 	 */
@@ -33,8 +44,15 @@ char* FATFS_GetLogicalPath();
 /* Read a string from file
  * path: The Volume id (0: or others)
  * namefile: File Name and it's extension
- * outbuffer: A buffer for the management of the result
+ * buffer_rx: A buffer for the management of the result
+ * size: size read
  */
-FRESULT FATFS_CharReadFromFile(char* path, char* namefile, char* outbuffer);
+FRESULT FATFS_CharReadFromFile(char* path, char* namefile, char* buffer_rx,int* size);
 
+/* Read a string from file
+ * path: The Volume id (0: or others)
+ * namefile: File Name and it's extension
+ * buffer_tx: A buffer for the transmitted data
+ */
+FRESULT FATFS_CharWriteToFile(char* path, char* namefile, char* buffer_tx);
 #endif
--- Src/fatfs.c
+++ Src/fatfs.c
@@ -32,23 +32,47 @@ char* FATFS_GetLabel(){
 	return str;
 }
 
-FRESULT FATFS_CharReadFromFile(char* path, char* namefile, char* outbuffer){
-	char* line = (char*) malloc(sizeof(char)*80); /* Line buffer */
+FRESULT FATFS_CharWriteToFile(char* path, char* namefile, char* buffer_tx){
+	FRESULT create;    /* FatFs return code */
+
+	/* Register work area to the default drive */
+	FRESULT mount = f_mount(&FatFs, path, 0);
+	UINT byte_count;
+	if(mount == FR_OK){
+		/* Open a file */
+		create = f_open(&file_handler, namefile, CREATE_NEW | WR);
+		if (create == FR_OK){
+			FRESULT write_status = f_write(&file_handler,buffer_tx,sizeof(buffer_tx),&byte_count);
+
+			/* Close the file */
+			return f_close(&file_handler);
+			}
+		}
+		else
+			return mount;
+}
+
+FRESULT FATFS_CharReadFromFile(char* path, char* namefile, char* buffer_rx, int* size){
 	FRESULT fr;    /* FatFs return code */
 
 	/* Register work area to the default drive */
 	FRESULT mount = f_mount(&FatFs, path, 0);
 	if(mount == FR_OK){
 		/* Open a file */
-		fr = f_open(&file_handler, namefile, FA_READ);
+		fr = f_open(&file_handler, namefile, RD);
 		if (fr)
 			return FR_NO_FILE;
 
-		/* Read all lines */
-		while(f_gets(line, file_handler.fsize, &file_handler));
-			memcpy(outbuffer,line, file_handler.fsize);
-			free(line);
+		uint32_t sizeread = (uint32_t) file_handler.fsize +1;
+		*size = sizeread;
+		char* line = (char*) malloc(sizeread*sizeof(char));
 
+		/* Read all lines */
+		 while(f_eof(&file_handler) == 0){
+			 f_gets(line, sizeread, &file_handler);
+		 }
+		memcpy(buffer_rx,line,sizeread*sizeof(char));
+		free(line);
 
 		/* Close the file */
 		return f_close(&file_handler);
